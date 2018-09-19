74d6c73148ca1ca2dea52fb93a037945c30b448c
Aldo Bergamini <aaberga@gmail.com>
2018-09-19 17:21:13 +0100

    Initial Commit

---
+++ Weather 5D/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather 5D
+//
+//  Created by Aldo Bergamini on 19/09/2018.
+//  Copyright © 2018 iBat Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
